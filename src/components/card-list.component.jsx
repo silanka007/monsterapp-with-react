d31ea147a25258406418b9fd58051892a0b1c4d4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-01-31 20:34:22 +0100

    creation of card-list and card component

---
+++ src/components/card-list.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './card.component';
+
+const CardList = props => (
+   <div className="card-list">
+      {props.monsters.map(monster =>
+         <Card key={monster.id} monster = {monster} />
+      )}
+   </div>
+)
+export default CardList;
---
+++ src/components/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = props => (
+    <div className="card-container">
+        <h1>
+        {props.monster.name} 
+        </h1>
+    </div>
+)
+
+export default Card;
